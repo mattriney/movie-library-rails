7711b8fa7023730d226887a8b7cd05934434ce1a
mattriney <61891660+mattriney@users.noreply.github.com>
2022-03-21 13:59:49 -0500

    movie creation method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,10 +9,11 @@ class MoviesController < ApplicationController
     end
 
     def new
-
     end
 
     def create
-
+        @movie = Movie.new(title: params[:title], description: params[:description])
+        @movie.save
+        redirect_to @movie
     end
 end
